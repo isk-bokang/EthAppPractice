0c925c59563c1932d845ecc5919f068ada208d8b
isk-bokang <bo.kang@iskra.world>
2022-05-24 10:08:38 +0900

    change address of token

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,6 +1,6 @@
 // This address points to a dummy ERC-20 contract. Replace it with your own smart contracts.
 const addresses = {
   ceaErc20: "0xa6dF0C88916f3e2831A329CE46566dDfBe9E74b7",
-  MET : "a072ace4ca8576acc8035c0ea13839ca",
+  MET : "0xF485Fc41AB35046D3551A00d26AB71A1e19D2342",
 };
 export default addresses;
