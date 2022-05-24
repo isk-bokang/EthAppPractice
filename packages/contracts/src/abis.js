c3678b1e9f82540b9e3ec161141484e7ab98072c
isk-bokang <bo.kang@iskra.world>
2022-05-24 09:37:45 +0900

    add MET ABI

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,9 +1,11 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
+import metAbi from "./abis/MyErcToken.json";
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
+  MET : metAbi.abi
 };
 
 export default abis;
