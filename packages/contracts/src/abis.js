61c9e9fe5d21e0eecc906761025b2a63e26e40c6
isk-bokang <bo.kang@iskra.world>
2022-05-24 11:24:18 +0900

    Add NFT Information (abi, address)

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,11 +1,13 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
 import metAbi from "./abis/MyErcToken.json";
+import metNftAbi from "./abis/MyErcNFT_Enumerable.json"
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
-  MET : metAbi.abi
+  MET : metAbi.abi,
+  METNFTE : metNftAbi.abi,
 };
 
 export default abis;
